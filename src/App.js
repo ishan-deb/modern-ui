a9980385db06329f009415edeb2934863acdc875
circular-biscuit <ishandeb@live.com>
2022-04-03 16:36:27 +0530

    added gradient background for header and navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { Brand, CTA, Navbar} from './components'
 function App() {
   return (
     <div className="App">
-      
+      <div className="gradient__bg">
+        <Header />
+        <Navbar />
+      </div>
     </div>
   );
 }
